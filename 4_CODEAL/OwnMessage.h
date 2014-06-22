3c0b13de712e0592e8886f834aee9a83d91843ec
Takayuki Sakai <kaky0922@gmail.com>
2014-06-22 17:13:11 +0900

    make OwnMessage.h and OwnMessage.m

---
+++ 4_CODEAL/OwnMessage.h
@@ -0,0 +1,13 @@
+//
+//  OwnMessage.h
+//  4_CODEAL
+//
+//  Created by Takayuki Sakai on 6/22/14.
+//  Copyright (c) 2014 CODEAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OwnMessage : UIView
+
+@end
