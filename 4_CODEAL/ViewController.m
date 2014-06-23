8b5347670f3467a38a56424037bfadfd9da9089b
Takayuki Sakai <kaky0922@gmail.com>
2014-06-23 14:28:03 +0900

    add scrollView to Storyboard

--- 4_CODEAL/ViewController.m
+++ 4_CODEAL/ViewController.m
@@ -11,6 +11,8 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
 @end
 
 @implementation ViewController
@@ -19,12 +21,16 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    OwnMessage *ownMessageView = [[OwnMessage alloc] init];
-    ownMessageView.frame = CGRectMake(0, 30, 320, 140);
-    //ownMessageView.frame = CGRectMake(ownMessageView.superview.bounds.size.width - 320, 30, 320, 140);
-    NSLog(@"%f", ownMessageView.superview.bounds.size.width - 320);
-    [ownMessageView setMessage:@"Hello method!"];
-    [self.view addSubview:ownMessageView];
+    for (int i = 0; i < 5; i++) {
+        OwnMessage *ownMessageView = [[OwnMessage alloc] init];
+        ownMessageView.frame = CGRectMake(0, 30 + 150 * i, 320, 140);
+        NSLog(@"%f", self.scrollView.bounds.size.width);
+        [ownMessageView setMessage:@"Hello method!"];
+        [self.scrollView addSubview:ownMessageView];
+        
+        //[self.scrollView addConstraint:[NSLayoutConstraint constraintWithItem:ownMessageView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.scrollView attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0.0]];
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning
