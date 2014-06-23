b40db063f8cf9a04ef57cb0a3e1a0d1add4eb886
Takayuki Sakai <kaky0922@gmail.com>
2014-06-23 15:27:59 +0900

    create OtherMessage.h, .m, .xib

---
+++ 4_CODEAL/OtherMessage.h
@@ -0,0 +1,13 @@
+//
+//  OtherMessage.h
+//  4_CODEAL
+//
+//  Created by Takayuki Sakai on 6/23/14.
+//  Copyright (c) 2014 CODEAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OtherMessage : UIView
+
+@end
---
+++ 4_CODEAL/OtherMessage.m
@@ -0,0 +1,31 @@
+//
+//  OtherMessage.m
+//  4_CODEAL
+//
+//  Created by Takayuki Sakai on 6/23/14.
+//  Copyright (c) 2014 CODEAL. All rights reserved.
+//
+
+#import "OtherMessage.h"
+
+@implementation OtherMessage
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
